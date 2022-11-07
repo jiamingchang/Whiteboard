eec42a62740ec70e453e4c5bfd70ff370314b6e3
MOSIAW <1197367393@qq.com>
2022-11-08 02:43:22 +0800

    fix: 修复路由bug

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -29,15 +29,16 @@ const router = createRouter({
 });
 
 // 对除了登录页外的进行全局守卫
-// router.beforeEach(async (to, from, next) => {
-//   if (to.path !== "/login") {
-//     let { isLogin } = store.state;
-//     if (!isLogin) {
-//       ElMessage.error("还未登录哦");
-//       next("/login");
-//     } else return;
-//   }
-//   next();
-// });
+router.beforeEach(async (to, from, next) => {
+  if (to.path !== "/") {
+    let { isLogin } = store.state;
+    if (!isLogin) {
+      ElMessage.error("还未登录哦");
+      next("/");
+      return;
+    }
+  }
+  next();
+});
 
 export default router;
