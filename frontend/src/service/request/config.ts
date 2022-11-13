a56c1448f793024df0157bcb31c15dc650efd1a7
kangpingan <kangpingan@taou.com>
2022-11-13 22:05:04 +0800

    feat: 接入服务器

--- frontend/src/service/request/config.ts
+++ frontend/src/service/request/config.ts
@@ -1,6 +1,6 @@
 let BASE_URL = "https://hm.sztufsrlab.com";
-let UPLOAD_URL = "http://localhost:4001";
-// let UPLOAD_URL = "http://182.61.20.203:4000";
+// let UPLOAD_URL = "http://localhost:4001";
+let UPLOAD_URL = "http://110.41.10.96:4001";
 const TIME_OUT = 100000;
 
 export { BASE_URL, TIME_OUT, UPLOAD_URL };
